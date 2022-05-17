cf34523028ce5984d158f33a050cf82af5ec0f9a
Newt <dragonminer101@gmail.com>
2022-05-17 14:07:40 +0100

    Add additional task and is_even.py solution

---
+++ histogram.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Standard input consists of a sequence of exactly N non-negative integers.
+
+# Example standard input
+# 3 <- number of lines to follow
+# 2
+# 1
+# 10
+
+# Example standard output
+# **
+# *
+# **********
+
+# Tip
+# The first output line contains two asterisks because the first input is 2.
+# The second output line contains one asterisk because the second input is 1.
+# And so on. The histogram is on its side.
--- is_even.py
+++ is_even.py
@@ -16,3 +16,13 @@
 
 # Example standard output 2
 #   False
+
+n = int(input())
+num = n - (n // 2) * 2
+# n // 2 will equal to either n / 2 in the case of even,
+# or n / 2 - .5 in the case of odd
+# we can multiply this calculated value by 2, which will sum to either
+# n or n - 1
+# n - n = 0, therefore anything that evaluates to 0 is even
+# n - (n - 1) = 1, therefore anything that evaluates to 1 is odd
+print(num == 0)
