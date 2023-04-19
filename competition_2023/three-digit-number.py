3e1996074120494e966685e85a446db20ee88029
ctyblue <ctyblue@winlabs.computing.dcu.ie>
2023-04-19 09:56:37 +0100

    add one more problem

---
+++ competition_2023/three-digit-number.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Standard input consists of exactly three digits,
+# one per line. Take these to be, respectively,
+# the first, second and third digits of a three-digit number.
+
+# Write a python script named three-digit-number.py
+# which writes that number to standard output.
+
+# example input:
+# 7
+# 3
+# 8
+
+# example output:
+# 738
